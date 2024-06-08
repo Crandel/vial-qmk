705ebf2707bd63d315e4ec9da317f3163a6d6c44
Vitalii Drevenchuk <4005032+Crandel@users.noreply.github.com>
2024-06-08 22:57:30 +0200

    Add wylderbuilds and skree

---
+++ keyboards/skree/trackball/vitalii/rules.mk
@@ -0,0 +1,13 @@
+SERIAL_DRIVER = vendor
+
+RGBLIGHT_ENABLE = no
+SLEEP_LED_ENABLE = no
+RGB_MATRIX_ENABLE = yes
+#CONSOLE_ENABLE = no
+##COMMAND_ENABLE = yes
+#BOOTMAGIC_ENABLE = yes
+#NKRO_ENABLE = yes           # Enable N-Key Rollover
+#
+#EXTRAKEY_ENABLE = yes       # Audio control and System control
+#
+#OPT_DEFS += -DHAL_USE_I2C=TRUE
