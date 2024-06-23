3860aea8a65a38f9132170b68d2d4a22de9c403a
Vitalii Drevenchuk <4005032+Crandel@users.noreply.github.com>
2024-06-23 23:45:33 +0200

    wylder update

--- keyboards/wylder/trackball/3x5_track/rules.mk
+++ keyboards/wylder/trackball/3x5_track/rules.mk
@@ -1,9 +1,9 @@
 SERIAL_DRIVER = vendor
 RGBLIGHT_ENABLE = no
-CONSOLE_ENABLE = no
+# CONSOLE_ENABLE = no
 #COMMAND_ENABLE = yes
-EXTRAKEY_ENABLE = yes       # Audio control and System control
-NKRO_ENABLE = yes           # Enable N-Key Rollover
+# EXTRAKEY_ENABLE = yes       # Audio control and System control
+# NKRO_ENABLE = yes           # Enable N-Key Rollover
 OLED_ENABLE = NO
 WPM_ENABLE = NO
 ENCODER_MAP_ENABLE = no
