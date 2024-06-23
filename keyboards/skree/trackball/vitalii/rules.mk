55e69ffa781a490d1c8d6390c1164aaa2b08bd88
Vitalii Drevenchuk <4005032+Crandel@users.noreply.github.com>
2024-06-23 23:45:33 +0200

    wylder update

--- keyboards/skree/trackball/vitalii/rules.mk
+++ keyboards/skree/trackball/vitalii/rules.mk
@@ -6,8 +6,8 @@ RGB_MATRIX_ENABLE = no
 #CONSOLE_ENABLE = no
 ##COMMAND_ENABLE = yes
 #BOOTMAGIC_ENABLE = yes
-NKRO_ENABLE = yes           # Enable N-Key Rollover
+# NKRO_ENABLE = yes           # Enable N-Key Rollover
 #
-EXTRAKEY_ENABLE = yes       # Audio control and System control
+# EXTRAKEY_ENABLE = yes       # Audio control and System control
 #
 #OPT_DEFS += -DHAL_USE_I2C=TRUE
